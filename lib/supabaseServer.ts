4bf0efdf0d6c9c93fbcaa32257b5a94a87cef1d1
akopaydaotan <kaylsmith27@gmail.com>
2025-12-17 13:18:23 +0800

    new version and server

--- lib/supabaseServer.ts
+++ lib/supabaseServer.ts
@@ -1,10 +1,25 @@
-import { createServerClient } from "@supabase/auth-helpers-nextjs";
+// lib/supabaseServer.ts
+import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
 export function supabaseServer() {
+  const cookieStore = cookies();
+
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    { cookies }
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value;
+        },
+        set(name: string, value: string, options: any) {
+          cookieStore.set({ name, value, ...options });
+        },
+        remove(name: string, options: any) {
+          cookieStore.set({ name, value: "", ...options });
+        },
+      },
+    }
   );
 }
